36a73aae48bad14d7bc918bbc97b4c34569d89f6
Pedro Soares Dias <pedroferraridias@gmail.com>
2022-10-22 19:10:00 -0300

    Atualização do Desafio

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
-﻿using DesafioPOO.Models;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
 
